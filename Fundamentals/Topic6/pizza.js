fb4be80fe70e1162474f1ddf9d3024a94b4079cd
rahimlisarkhan <whelp@Whelps-MacBook-Air.local>
2023-10-04 10:54:19 +0400

    update

---
+++ Fundamentals/Topic6/pizza.js
@@ -0,0 +1,15 @@
+var name_pizza_rest = "Pizzahut";
+var pizza_count = 20;
+var is_hungry = false;
+var pizza_cost = 15;
+var total_cost = pizza_count * pizza_cost;
+
+// ================================ alert List ================================ //
+
+alert("Welcome: " + name_pizza_rest);
+
+alert(`Pizza cost is ${pizza_cost}$ each pizza`);
+
+alert(`Your total is:  ${total_cost}$`);
+
+alert(`Are you still hungry? ${is_hungry}`);
