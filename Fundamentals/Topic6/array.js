684f2fbf5099946d0aafc571f18444d9897b520a
rahimlisarkhan <whelp@Whelps-MacBook-Air.local>
2023-10-06 00:05:15 +0400

    update

---
+++ Fundamentals/Topic6/array.js
@@ -0,0 +1,63 @@
+var a = 5;
+
+var b = 4;
+
+var c = 2;
+
+var f = "fullname";
+
+var list = [5, 4, 99, "fullname"]; //? Array
+
+// var result = list[3];
+
+// var a = 5
+
+// a = 12
+
+// console.log(a); //12
+
+// list[2] = 101;
+
+// console.log(list);
+
+// console.log(list[2]);
+
+// var a = 500;
+
+// var b = a + 50;
+
+// a = 0;
+
+// console.log("b deyeri", b);
+// console.log("a deyeri", a);
+
+// var c = "Rest";
+
+// var d = c;
+
+// c = "R";
+
+// console.log(c);
+// console.log(d);
+
+// ("| R | | | REST | |"); //? STACK
+
+// var arr1 = ["Mango", "Pie", "Watermaloon", "Banana", "Ezgil"]; //? HEAP
+
+// var arr2 = arr1;
+
+// arr1[1] = "Apple";
+
+// console.log("arr1", arr1);
+// console.log("arr2", arr2);
+
+var arr1 = [1, 2, 3, 4, 5, 6];
+
+// var result = arr1[1] + arr1[5];
+
+// arr1[6] = 101;
+// arr1[10] = 101;
+
+console.log(arr1);
+
+// console.log(result);
--- Fundamentals/Topic6/index.js
+++ Fundamentals/Topic6/index.js
@@ -1,232 +1,59 @@
-//? Primitive
-//? Number
-//? String
-//? Boolean
-//? Null
-//? Undefined
+//? Array
+// var arr1 = [12, 20, 25, 30, 45];
 
-var age = 24; //? integer
-var productPrice = 7.9; //? float
+//! PUSH
+// arr1.push(105);
+// arr1.push(45);
 
-var fullname = "John Doe";
-var position = "Developer";
-var about = `Lorem ipsum color`;
+//! POP
+// var sildiyi = arr1.pop();
 
-var married = false;
-var access = true;
+//! Shift and Unshift
+// arr1.shift();
+// arr1.unshift(103);
+// arr1.unshift(11);
 
-// var imageUrl = "https://dsdass.com/g.jpg";
-// var imageUrl = null;
+//! JOIN
+// var convertString = arr1.join(" ");
 
-// var description = undefined;
-
-// var num1 = 11;
-
-// var num2 = num1;
-
-// num1 = 15;
-
-// console.log(num1);
-// console.log(num2);
-
-var a = 7;
-
-// a = a + 12;
-// a += 12;
-a *= 12;
-// a -= 12;
-
-console.log(a);
-
-// var b = 88;
-
-// var c = a * (b - 2);
-
-// console.log(c);
-
-// var b = a + 4;
-// var b = a - 2;
-// var b = a * 2;
-// var b = a / 2;
-// var b = a % 2;
-// var b = a ** 2;
-
-// var b = a + "2";
-// var b = a - "2";
-
-// console.log(b);
-
-var a = 5;
-
-// var b = "5";
-var b = 3;
-
-var result = a == b;
-// var result = a != b;
-// var result = a === b;
-// var result = a !== b;
-
-// var result = a > b;
-// var result = a < b;
-// var result = a <= b;
-
-// console.log("result", result);
-
-var age = 14;
-
-// if (age >= 18) {
-//   console.log("Cix sinifden!");
-// } else {
-//   console.log("Kunce get");
-// }
-
-// if (result == true) {
-
-// if (a == b) {
-//   console.log("beraberdir");
-// } else {
-//   console.log("NOT!");
-// }
-
-// var personal = "Marry";
-
-// var age = 27;
-
-// if (age >= 18) {
-//   var result = `Buyur kec iceri ${personal}. Cunki senin yasin ${age}-dir`;
-
-//   console.log(result);
-// } else {
-//   var result = "Qadagandir";
-//   console.log(result);
-// }
-
-// const number1 = 31;
-// const number2 = 2;
-
-// const age = number1 % number2;
-
-// const num = 32;
-
-// if (num % 2 == 0) {
-//   console.log("Even number");
-// } else {
-//   console.log("Odd number");
-// }
-
-// var result = num % 2;
+//! includes & indexOf
+// var result = arr1.includes(25);
+// var result = arr1.indexOf(105);
 
 // console.log(result);
 
-// var emptyString = "";
-// var zero = 0;
-// var a = null;
-// var b = undefined;
-// var bol = false;
+// if(result ==true){
 
-var citizen = "az";
+// }else{
 
-var married = true;
+// }
 
-// var result = married === false && age >= 18 && citizen === "az";
-var result = married === false || age >= 18 || citizen === "az";
+// if(result !==-1){
 
-// console.log("result", result);
+// }else{
 
-// if (married === false || age >= 18 || citizen === "az") {
-//   console.log("Derse xos geldin!");
+// }
+// console.log("sildiyi", sildiyi);
+// console.log("convertString", convertString);
+
+var arr1 = [12, 20, 25, 30, 45, 101, 102, 105, 1001];
+
+// var lastElementIndex = arr1.length - 1;
+// var lastEl = arr1[lastElementIndex];
+
+// console.log("lastEl", lastEl);
+
+// if (arr1.length > 1) {
+//   console.log("Bos yerimiz yoxdu");
 // } else {
-//   console.log("Biz sizle elaqe saxlayacagiq");
+//   console.log("Vakansiya var");
 // }
 
-// if (age >= 18) {
-//   console.log("big bro");
-// }
+// arr1.splice(4, null, 109, "Banana", "Cake");
+// arr1.splice(arr1.length - 3, 3);
+// arr1.splice(2, 3);
+arr1.splice(2, 1, 210, 310, 400);
 
-// if (age >= 18) {
-//   console.log("big bro");
-// } else {
-//   console.log("Hmm");
-// }
+// arr1.splice(2, 0, 210, 310, 400);
 
-// var age = 15;
-
-// if (age >= 18) {
-//   console.log("18den boyukdur!");
-// } else if (age < 18 && age > 14) {
-//   console.log("Uygun ola biler!");
-// } else if (age < 14 && age > 11) {
-//   console.log("Uygun ola biler!");
-// } else {
-//   console.log("Uygun yerimiz yoxdur");
-// }
-
-// alert("Hey");
-
-// confirm("Eminsenmi?");
-// var result = prompt("Imtahan neticevizi yazin");
-
-// var result = 110;
-
-// if (result >= 80 && result <= 100) {
-//   console.log("Woow.Siz A aldiz");
-// } else if (result >= 70 && result < 80) {
-//   console.log("Hmmm.Siz B aldiz");
-// } else if (result >= 60 && result < 70) {
-//   console.log("Hmmm.Siz C aldiz");
-// } else if (result >= 50 && result < 60) {
-//   console.log("Hmmm.Siz D aldiz");
-// } else if (result > 0 && result < 50) {
-//   console.log("Hmmm.Siz F aldiz");
-// } else {
-//   console.log("Daxil etdiyiniz netice duzgun deyil");
-// }
-
-// var userAge = prompt("Yasinizi daxil edin");
-
-// if (userAge >= 18) {
-//   console.log("Uniersitet vaxtidi");
-// } else if (userAge == 16) {
-//   console.log("Hazirliqlara baslamalisan!");
-//   alert("Hazirliqlara baslamalisan!");
-// } else {
-//   console.log("Narahat olma");
-// }
-
-// var q1 = confirm("Imtahana hazirlasirsan?");
-
-// console.log(q1);
-
-var q1 = confirm("Goy uzunun rengi mavidirmi?");
-
-// if(q1)
-//! 1ci sert
-//! -----------------------------
-if (q1 == true) {
-  console.log("Aferin ugurlu oldu!");
-
-  var q2 = prompt("Yarpagin rengi nedir?");
-
-  //! 2ci sert
-  //! -----------------------------
-  if (q2 == "yasil") {
-    console.log("Aferin ugurlu oldu! Sizin xaliniz:20");
-
-    var q3 = prompt("Doner xosluyursan?");
-
-    //! 3ci sert
-    //! -----------------------------
-    if (q2 == "yasil") {
-      console.log("Aferin ugurlu oldu! Sizin xaliniz:20");
-    } else {
-      console.log("Sizin xal:10");
-    }
-    //! -----------------------------
-  } else {
-    console.log("Sizin xal:10");
-  }
-  //! -----------------------------
-} else {
-  console.log("Sizin xal:0");
-}
-//! -----------------------------
+console.log(arr1);
